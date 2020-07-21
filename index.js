e1099c87e1a848257c09d5f5aca16b9c559a3913
thomasSDK <tsdkelly@protonmail.com>
2020-07-21 18:24:51 +0100

    artifact upload branch
    stalls on zipping files on machine

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 const core = require("@actions/core");
 const exec = require("@actions/exec");
+const artifact = require("@actions/artifact");
 
 const BUILDPLATFORM = core.getInput("BuildPlatform");
 const BUILDCONFIGURATION = core.getInput("BuildConfiguration");
 const BUILDDIRECTORY = core.getInput("BuildDirectory");
 const PROJECT = core.getInput("Project");
+const UploadArtifact = core.getInput("UploadArtifact");
 
 const BuildSolution = `${PROJECT}.visualstudio\\${PROJECT}.sln`;
 
 async function run() {
   try {
+    // tsdk: causes the action to hang
     // await exec.exec("cmd", ["set"]);
     await exec.exec("MSBuild", [
       BuildSolution,
       `/property:Configuration=${BUILDCONFIGURATION}`,
       `/property:Platform=${BUILDPLATFORM}`,
     ]);
+
+    if (UploadArtifact) {
+      const artifactClient = artifact.create();
+      const artifactName = "windows";
+
+      await exec.exec(
+        "Compress-Archive",
+        ["-LiteralPath", `${BUILDDIRECTORY}.zip`, `DestinationPath`, `${BUILDDIRECTORY}`],
+        { cwd: "Build" }
+      );
+
+      const files = [`Build/${BUILDDIRECTORY}.zip`];
+      const rootDirectory = ".";
+      const options = {
+        continueOnError: true,
+      };
+
+      const uploadResult = await artifactClient.uploadArtifact(
+        artifactName,
+        files,
+        rootDirectory,
+        options
+      );
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
