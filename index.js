f3b55375dd66a02d61c8455c72bd5a7f35a5c46f
Graham Reeves <graham@newchromantics.com>
2020-08-18 16:17:15 +0100

    Added nuget restore as a step before building

--- index.js
+++ index.js
@@ -5,11 +5,27 @@ const BUILDPLATFORM = core.getInput("BuildPlatform");
 const BUILDCONFIGURATION = core.getInput("BuildConfiguration");
 const BUILDDIRECTORY = core.getInput("BuildDirectory");
 const PROJECT = core.getInput("Project");
-
-const BuildSolution = `${PROJECT}.visualstudio\\${PROJECT}.sln`;
+const BuildSolution = core.getInput("BuildSolution") || `${PROJECT}.visualstudio\\${PROJECT}.sln`;
 
 async function run() {
   try {
+    
+    //  execute nuget restore to restore any packages (if packages.config is present) in the solution
+    //  nuget isn't automatically installed (and it's NOT in the github runners) so this can fail
+    //  Show a prompt if it fails
+    try
+    {
+      console.log(`Restoring nuget packages...`);
+      await exec.exec("nuget restore %BuildSolution%", ['restore',BuildSolution] );
+    }
+    catch(e)
+    {
+      console.warn(`nuget restore ${BuildSolution} failed: ${e}`);
+      console.warn(`Perhaps nuget isn't installed. Use this in your workflow to integrate it;`);
+      console.log(`- name: Download Nuget`);
+      console.log(`  uses: warrenbuckley/Setup-Nuget@v1`);
+    }      
+    
     // await exec.exec("cmd", ["set"]);
     await exec.exec("MSBuild", [
       BuildSolution,
